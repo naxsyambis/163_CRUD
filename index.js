599f56b8527e2b8cd2bad049eebab3ba9e96ee00
naxsyambis <naxsyambis@gmail.com>
2025-10-21 14:38:15 +0700

    Tambah endpoint root dan jalankan server

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
+
